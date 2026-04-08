b89fb935b307837dae876ddc640bebcbd46f57ad
Ankur <ankur.teksun@gmail.com>
2026-04-08 20:03:45 +0530

    add - nothing

--- user_app/urls.py
+++ user_app/urls.py
@@ -5,3 +5,8 @@ url_patterns = [
         path("user-register/", UserRegisterAPI.as_view(), name="Register-user"),
         path("user-login/",UserLoginAPI.as_view(), name="user-login"),
 ]
+
+
+
+
+
