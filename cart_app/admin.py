2dcadac0dce3ebc9379c9774f400bfd97355e0b5
hitkacha <hitkacha15@gmail.com>
2026-04-16 19:45:33 +0530

    admin add

--- cart_app/admin.py
+++ cart_app/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from cart_app.models import PromoCode
+from cart_app.models import PromoCode, Cart, CartItem
 
 
 # Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["id","user"]
+
+
 @admin.register(PromoCode)
 class PromoCodeAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "value", "type"]
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ["id","cart","product","quantity"]
--- order_app/admin.py
+++ order_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from order_app.models import Address, OrderItem, Order
+
+
 # Register your models here.
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ["id","user","street","area","society_name","landmark","city","state","pincode","location"]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["id","user","quantity","product"]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id","address","total_amount","payment_method","status","discount","promo_code","order_items"]
