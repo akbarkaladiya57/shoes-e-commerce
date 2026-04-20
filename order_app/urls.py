a692bee0caaa8218de4808b828fbe5f215a72337
hitkacha <hitkacha15@gmail.com>
2026-04-20 11:02:50 +0530

    update order api

--- order_app/urls.py
+++ order_app/urls.py
@@ -1,14 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from order_app.views import AddressAPI, OrderItemAPI
-from django.urls import path
 from order_app.views import OrderAPI
 
 router = DefaultRouter()
 router.register(r'address', AddressAPI, basename='addresses')
 router.register(r'order-items',OrderItemAPI,basename='order-items')
+router.register(r'order-create',OrderAPI,basename='order-create')
 
-urlpatterns = [
-    path('order/create/', OrderAPI.as_view(), name='order-list-create'),
-]
-
-urlpatterns += router.urls
+urlpatterns = router.urls
--- order_app/views.py
+++ order_app/views.py
@@ -79,7 +79,57 @@ class OrderItemAPI(viewsets.ModelViewSet):
             "message": "Item deleted successfully"
         }, status=HTTP_200_OK)
 
-class OrderAPI(ListCreateAPIView):
-    queryset = Order.objects.all()
+
+
+class OrderAPI(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(address__user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response({
+            "success": True,
+            "message": "Order created successfully",
+            "data": serializer.data
+        }, status=HTTP_201_CREATED)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        return Response({
+            "success": True,
+            "data": serializer.data
+        }, status=HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({
+            "success": True,
+            "message": "Order updated successfully",
+            "data": serializer.data
+        }, status=HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+
+        return Response({
+            "success": True,
+            "message": "Order deleted successfully"
+        }, status=HTTP_200_OK)
